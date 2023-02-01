ac0586468dc81f15f9897fea867bdb124889afa0
Payal talmale <payaltalmale07@gmail.com>
2023-02-02 02:04:27 +0530

    message

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # pipeline-ml
 '''
-conda create -p venv python== 3.7 -y
+conda create -p venv python==3.7 -y
+'''
+'''
+conda activate venv/
 '''
---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+from typing import List
+
+
+PROJECT_NAME="housing-prediction"
+VERSION="0.0.1"
+AUTHOR="payal talmale"
+DESCRIPTION="this is the first   machine learning project"
+PACKEGES=["housing"]
+REQUIREMENT_FILE_NAME="requirements.txt"
+
+
+def get_requirements_list()->List[str]:
+    with open(REQUIREMENT_FILE_NAME) as requirement_file:
+        return requirement_file.readlines()
+
+setup(
+name=PROJECT_NAME,  
+    version=VERSION,
+    author=AUTHOR,
+    description=DESCRIPTION,
+    packeges=PACKEGES,
+    install_requires=get_requirements_list()
+)
+
+
+if __name__=="__main__":
+    print(get_requirements_list())
